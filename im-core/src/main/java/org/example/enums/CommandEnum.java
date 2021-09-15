42f74f05e0682fb9ee32fc7cecfb6441d176d878
brucelee <473302042@qq.com>
2021-09-15 21:00:36 +0800

    chushihua

--- im-core/src/main/java/org/example/enums/CommandEnum.java
+++ im-core/src/main/java/org/example/enums/CommandEnum.java
@@ -8,24 +8,165 @@ import lombok.Getter;
 public enum CommandEnum {
     COMMAND_UN_KNOW(0),
     /**
-     * 握手请求
+     * <pre>
+     *握手请求，含http的websocket握手请求
+     * </pre>
+     *
+     * <code>COMMAND_HANDSHAKE_REQ = 1;</code>
      */
     COMMAND_HANDSHAKE_REQ(1),
-
     /**
-     * 握手相应
+     * <pre>
+     *握手响应，含http的websocket握手响应
+     * </pre>
+     *
+     * <code>COMMAND_HANDSHAKE_RESP = 2;</code>
      */
     COMMAND_HANDSHAKE_RESP(2),
-
     /**
-     * 登录请求
+     * <pre>
+     *鉴权请求
+     * </pre>
+     *
+     * <code>COMMAND_AUTH_REQ = 3;</code>
+     */
+    COMMAND_AUTH_REQ(3),
+    /**
+     * <pre>
+     * 鉴权响应
+     * </pre>
+     *
+     * <code>COMMAND_AUTH_RESP = 4;</code>
+     */
+    COMMAND_AUTH_RESP(4),
+    /**
+     * <pre>
+     *登录请求
+     * </pre>
+     *
+     * <code>COMMAND_LOGIN_REQ = 5;</code>
      */
     COMMAND_LOGIN_REQ(5),
-
     /**
-     * 登录响应
+     * <pre>
+     *登录响应
+     * </pre>
+     *
+     * <code>COMMAND_LOGIN_RESP = 6;</code>
      */
-    COMMAND_LOGIN_RESP(6)
+    COMMAND_LOGIN_RESP(6),
+    /**
+     * <pre>
+     *申请进入群组
+     * </pre>
+     *
+     * <code>COMMAND_JOIN_GROUP_REQ = 7;</code>
+     */
+    COMMAND_JOIN_GROUP_REQ(7),
+    /**
+     * <pre>
+     *申请进入群组响应
+     * </pre>
+     *
+     * <code>COMMAND_JOIN_GROUP_RESP = 8;</code>
+     */
+    COMMAND_JOIN_GROUP_RESP(8),
+    /**
+     * <pre>
+     *进入群组通知
+     * </pre>
+     *
+     * <code>COMMAND_JOIN_GROUP_NOTIFY_RESP = 9;</code>
+     */
+    COMMAND_JOIN_GROUP_NOTIFY_RESP(9),
+    /**
+     * <pre>
+     *退出群组通知
+     * </pre>
+     *
+     * <code>COMMAND_EXIT_GROUP_NOTIFY_RESP = 10;</code>
+     */
+    COMMAND_EXIT_GROUP_NOTIFY_RESP(10),
+    /**
+     * <pre>
+     *聊天请求
+     * </pre>
+     *
+     * <code>COMMAND_CHAT_REQ = 11;</code>
+     */
+    COMMAND_CHAT_REQ(11),
+    /**
+     * <pre>
+     *聊天响应
+     * </pre>
+     *
+     * <code>COMMAND_CHAT_RESP = 12;</code>
+     */
+    COMMAND_CHAT_RESP(12),
+    /**
+     * <pre>
+     *心跳请求
+     * </pre>
+     *
+     * <code>COMMAND_HEARTBEAT_REQ = 13;</code>
+     */
+    COMMAND_HEARTBEAT_REQ(13),
+    /**
+     * <pre>
+     *关闭请求
+     * </pre>
+     *
+     * <code>COMMAND_CLOSE_REQ = 14;</code>
+     */
+    COMMAND_CLOSE_REQ(14),
+    /**
+     * <pre>
+     *发出撤消消息指令(管理员可以撤消所有人的消息，自己可以撤消自己的消息)
+     * </pre>
+     *
+     * <code>COMMAND_CANCEL_MSG_REQ = 15;</code>
+     */
+    COMMAND_CANCEL_MSG_REQ(15),
+    /**
+     * <pre>
+     *收到撤消消息指令
+     * </pre>
+     *
+     * <code>COMMAND_CANCEL_MSG_RESP = 16;</code>
+     */
+    COMMAND_CANCEL_MSG_RESP(16),
+    /**
+     * <pre>
+     *获取用户信息;
+     * </pre>
+     *
+     * <code>COMMAND_GET_USER_REQ = 17;</code>
+     */
+    COMMAND_GET_USER_REQ(17),
+    /**
+     * <pre>
+     *获取用户信息响应;
+     * </pre>
+     *
+     * <code>COMMAND_GET_USER_RESP = 18;</code>
+     */
+    COMMAND_GET_USER_RESP(18),
+    /**
+     * <pre>
+     * 获取聊天消息;
+     * </pre>
+     *
+     * <code>COMMAND_GET_MESSAGE_REQ = 19;</code>
+     */
+    COMMAND_GET_MESSAGE_REQ(19),
+    /**
+     * <pre>
+     * 获取聊天消息响应;
+     * </pre>
+     *
+     * <code>COMMAND_GET_MESSAGE_RESP = 20;</code>
+     */
+    COMMAND_GET_MESSAGE_RESP(20),
     ;
 
 
