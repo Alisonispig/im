5c8a4aeb550838b4ec597c1ff05fdaee4d8848fe
胡汉三 <473302042@qq.com>
2021-09-30 09:19:28 +0800

    IM

--- im-core/src/main/java/org/example/packets/ChatRepBody.java
+++ im-core/src/main/java/org/example/packets/ChatRepBody.java
@@ -5,10 +5,19 @@ import lombok.Data;
 @Data
 public class ChatRepBody {
 
+    /**
+     * 消息ID
+     */
     private Integer _id;
 
+    /**
+     * 消息内容
+     */
     private String content;
 
+    /**
+     *
+     */
     private String senderId;
 
 
