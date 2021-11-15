e50f25fc61908f79bf90d5c8e664f20a7f985adc
胡汉三 <473302042@qq.com>
2021-11-15 17:27:24 +0800

    文件历史消息

--- im-server/script/startup.sh
+++ im-server/script/startup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+cd `dirname  $0`
 APP_ENTRY=org.example.ImServer
-CP=./:/data/config:/data/lib/*
-java -Xverify:none -XX:+HeapDumpOnOutOfMemoryError -Dtio.default.read.buffer.size=512 -XX:HeapDumpPath=./java-im-server-pid.hprof -cp $CP -Dglobal.config.path=/data/ $APP_ENTRY
+CP=./:./config:./lib/*
+java -Xverify:none -XX:+HeapDumpOnOutOfMemoryError -Dtio.default.read.buffer.size=512 -XX:HeapDumpPath=./java-im-server-pid.hprof -Dglobal.config.path=/data/ -cp $CP  $APP_ENTRY
